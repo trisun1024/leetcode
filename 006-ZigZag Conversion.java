b1f4b9b5fcc25094287537050ca786d154f16d16
jing <bill.j.pang@gmail.com>
2019-05-24 19:23:14 -0400

    20190524

---
+++ 006-ZigZag Conversion.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1)
+            return s;
+
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        int clen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < len; j += clen) {
+                sb.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + clen - i < len)
+                    sb.append(s.charAt(j + clen - i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ 008-String to Integer (atoi).java
@@ -0,0 +1,33 @@
+class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if (str == null || str.length() == 0)
+            return 0;
+
+        char sign = '+';
+        int i = 0;
+        if (str.charAt(i) == '-') {
+            sign = '-';
+            i++;
+        } else if (str.charAt(i) == '+') {
+            i++;
+        }
+
+        double res = 0;
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+
+            res = res * 10 + (str.charAt(i) - '0');
+            i++;
+
+        }
+
+        if (sign == '-')
+            res = -res;
+
+        if (res > Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+        if (res < Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        return (int) res;
+    }
+}
