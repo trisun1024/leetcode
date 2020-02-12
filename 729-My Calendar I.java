4ad29915fa452dd100a00e07f9e9012048ff064e
jing <bill.j.pang@gmail.com>
2020-02-11 23:01:36 -0500

    20200211

--- 729-My Calendar I.java
+++ 729-My Calendar I.java
@@ -23,3 +23,21 @@ class MyCalendar {
  * Your MyCalendar object will be instantiated and called as such: MyCalendar
  * obj = new MyCalendar(); boolean param_1 = obj.book(start,end);
  */
+
+// brute force solution
+class MyCalendarII {
+    List<int[]> calendar;
+
+    MyCalendarII() {
+        calendar = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] iv : calendar) {
+            if (iv[0] < end && start < iv[1])
+                return false;
+        }
+        calendar.add(new int[] { start, end });
+        return true;
+    }
+}
