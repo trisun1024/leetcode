830cdb1d7088f0b26cc9825ccc8684ea001c76da
jing <bill.j.pang@gmail.com>
2020-04-27 15:46:53 -0400

    20200427

---
+++ 201-Bitwise AND of Numbers Range.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int rangeBitwiseAnd(int m, int n) {
+        int shift = 0;
+        // find the common 1-bits
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            ++shift;
+        }
+        return m << shift;
+    }
+
+    public int rangeBitwiseAndII(int m, int n) {
+        while (m < n) {
+            n = n & (n - 1);
+        }
+        return m & n;
+    }
+}
