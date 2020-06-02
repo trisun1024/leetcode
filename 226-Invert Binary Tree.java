af3cdf62ba7059949ca7c72b2b02dc68b6ae99e1
jing <jip45@pitt.edu>
2020-06-02 18:25:48 -0400

    20200601

--- 226-Invert Binary Tree.java
+++ 226-Invert Binary Tree.java
@@ -1,35 +1,47 @@
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
-// Recursive
+import java.util.*;
+
 class Solution {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int v) {
+            this.val = v;
+        }
+    }
+
+    // Recursive
     public TreeNode invertTree(TreeNode root) {
-        if (root == null)
-            return null;
-        TreeNode right = invertTree(root.right);
-        TreeNode left = invertTree(root.left);
-        root.left = right;
-        root.right = left;
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
         return root;
     }
-}
-// Iterative
-class Solution1 {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null)
-            return null;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
+
+    // Iterative
+    public TreeNode invertTreeII(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
         while (!queue.isEmpty()) {
             TreeNode cur = queue.poll();
             TreeNode tmp = cur.left;
             cur.left = cur.right;
             cur.right = tmp;
-            if (cur.left != null)
-                queue.add(cur.left);
-            if (cur.right != null)
-                queue.add(cur.right);
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
         }
         return root;
     }
