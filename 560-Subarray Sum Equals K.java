e4d76ffcd8c808db2654b21c32a845223492cb2d
jing <jip45@pitt.edu>
2020-06-04 14:35:35 -0400

    20200604

--- 560-Subarray Sum Equals K.java
+++ 560-Subarray Sum Equals K.java
@@ -4,13 +4,12 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         int[] prefix = new int[nums.length + 1];
         for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
+            prefix[i] = nums[i - 1] + prefix[i - 1];
         }
-        // System.out.println(Arrays.toString(prefix));
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length + 1; j++) {
-                if (prefix[j] - prefix[i] == k) {
+            for (int j = i + 1; j <= nums.length; j++) {
+                if ((prefix[j] - prefix[i]) == k) {
                     count++;
                 }
             }
