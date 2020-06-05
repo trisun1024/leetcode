169c8da40a873a8be6a77483e07fbe449dc573c3
jing <jip45@pitt.edu>
2020-06-04 22:20:28 -0400

    20200604

---
+++ 723-Candy Crush.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int[][] candyCrush(int[][] board) {
+        boolean flag = false;
+        int row = board.length; int col=board[0].length;
+        // explore horizontal, find consetive three
+        for(int i = 0 ; i < row; i++) {
+            for(int j = 0; j + 2 < col ; j++) {
+                int v = Math.abs(board[i][j]) ;
+                if(v!=0 && v==Math.abs(board[i][j+1]) && v==Math.abs(board[i][j+2])) {
+                    board[i][j] = board[i][j+1] = board[i][j+2] = -v;
+                    flag = true;
+                }
+            } 
+        }
+        // explore vertical, find consective three 
+        for(int i = 0; i+ 2 < row ; i++) {
+            for(int j = 0; j< col ; j++) {
+                int v = Math.abs(board[i][j]);
+                if(v!=0 && v ==Math.abs(board[i+1][j]) && v==Math.abs(board[i+2][j])) {
+                    board[i][j] = board[i+1][j] = board[i+2][j] = -v;
+                    flag = true;
+                }
+            }
+        }
+        // set all the negative value to zero, and move
+        for(int j = 0; j < col; j++) {
+            int right = row-1;
+            for(int i = row-1; i >=0 ; i--) {
+                if(board[i][j] >0) {
+                    board[right--][j] = board[i][j];
+                }
+            }
+            while(right >=0) {
+                board[right--][j] =0;
+            }
+        }
+        return flag ? candyCrush(board) : board;
+    }
+}
