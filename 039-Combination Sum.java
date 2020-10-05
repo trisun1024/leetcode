bd6d8acfd0acde7d6f57efc6d5650ef040252b65
jing <bill.j.pang@gmail.com>
2020-10-05 10:52:34 -0400

    20201003

--- 039-Combination Sum.java
+++ 039-Combination Sum.java
@@ -1,27 +1,43 @@
-
-// dynamic programming
 import java.util.*;
 
 class CombinationSum {
+ 
+    // DFS. 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> cur_res = new ArrayList<>();
-        Arrays.sort(candidates); // for possible early stop
-        backtrack(candidates, 0, target, cur_res, result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, cur, res);
+        // helperI(candidates, 0, target, cur, res);
+        return res;
+    }
+    
+    private void helper(int[] candidates, int index, int remain, List<Integer> cur, List<List<Integer>> res) {
+        if(remain < 0) {
+            return ;
+        }
+        if(remain == 0) {
+            res.add(new ArrayList<>(cur));
+        }
+        for(int i = index; i < candidates.length; i++) {
+            cur.add(candidates[i]);
+            helper(candidates, i, remain-candidates[i], cur, res);
+            cur.remove(cur.size()-1);
+        }
     }
 
-    private void backtrack(int[] nums, int start, int remain, List<Integer> cur_res, List<List<Integer>> result) {
-        if (remain < 0) // early stop
-            return;
-        else if (remain == 0)
-            result.add(new ArrayList<>(cur_res));
-        else {
-            for (int i = start; i < nums.length; i++) {
-                cur_res.add(nums[i]);
-                backtrack(nums, i, remain - nums[i], cur_res, result);
-                cur_res.remove(cur_res.size() - 1);
+    // Improve DFS. Early Stop. 
+    private void helperI(int[] candidates, int index, int remain, List<Integer> cur , List<List<Integer>> res) {
+        if(remain == 0) {
+            res.add(new ArrayList<>(cur));
+        }
+        for(int i = index; i < candidates.length; i++) {
+            if(remain - candidates[i] < 0) {
+                continue;
             }
+            cur.add(candidates[i]);
+            helper(candidates, i, remain-candidates[i], cur, res);
+            cur.remove(cur.size()-1);
         }
     }
 }
--- 457-Circular Array Loop.java
+++ 457-Circular Array Loop.java
@@ -4,76 +4,77 @@ class CircularArrayLoop {
 
     // Two Pointers.
     public boolean circularArrayLoop(int[] nums) {
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > 0) {
-                if (checkCircle(nums, i, len)) {
+        if (nums.length <= 1) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] %= nums.length;
+        }
+
+        boolean[] visited = new boolean[nums.length];
+
+        for (int i = 0; i < nums.length; ++i) {
+            int slow = i;
+            int fast = i;
+
+            if (visited[slow]) {
+                continue;
+            }
+
+            while (nums[slow] * nums[next(nums, fast)] > 0 && nums[slow] * nums[next(nums, next(nums, fast))] > 0) {
+                slow = next(nums, slow);
+                fast = next(nums, next(nums, fast));
+
+                if (visited[slow]) {
+                    break;
+                }
+
+                if (slow == fast) {
                     return true;
                 }
-            } else if (nums[i] < 0) {
-                if (checkCircle(nums, i, len)) {
-                    return true;
-                }
+                visited[slow] = true;
             }
         }
         return false;
     }
 
-    private boolean checkCircle(int[] nums, int start, int len) {
-        boolean positive = false;
-        if (nums[start] > 0) {
-            positive = true;
-        }
-        int slow = start;
-        int fast = start;
-        while ((nums[slow] > 0) == positive && (nums[fast] > 0) == positive
-                && (nums[getIndex(fast + nums[fast], len)] > 0) == positive) {
-            slow = getIndex(slow + nums[slow], len);
-            if (getIndex(slow + nums[slow], len) == slow) {
-                break;
-            }
-            fast = getIndex(fast + nums[fast], len);
-            fast = getIndex(fast + nums[fast], len);
-            if (nums[slow] == nums[fast])
-                return true;
-        }
-        return false;
-    }
-
-    private int getIndex(int input, int len) {
-        if (input >= 0)
-            return input % len;
-        return len - (len - input) % len;
+    private int next(int[] nums, int i) {
+        int n = nums.length;
+        return (i + nums[i] + n) % n;
     }
 
     // DFS.
     public boolean circularArrayLoopI(int[] nums) {
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] > 0 && dfs(nums, i, true, new HashSet<Integer>()))
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++)
+            if (!map.containsKey(i) && search(i, nums, map))
                 return true;
-            if (nums[i] < 0 && dfs(nums, i, false, new HashSet<Integer>()))
-                return true;
-        }
         return false;
     }
 
-    private boolean dfs(int[] nums, int curr, boolean isForward, Set<Integer> visited) {
-        if (visited.contains(curr))
-            return true; // 有环
-        if ((nums[curr] > 0) != isForward)
-            return false; // 符号不对
-        if (getIndex(curr + nums[curr], nums) == curr)
-            return false; // 环中只有一个元素
-        int next = getIndex(curr + nums[curr], nums);
-        visited.add(curr);
-        return dfs(nums, next, isForward, visited);
+    public boolean search(int i, int[] arr, HashMap<Integer, Integer> map) {
+        int slow = i, fast = i;
+        while (fast >= 0 && arr[fast] * arr[i] > 0 && next(fast, arr) >= 0 && arr[next(fast, arr)] * arr[i] > 0) {
+            if (map.getOrDefault(fast, i) != i)
+                return false;
+            map.put(fast, i);
+            slow = next(slow, arr);
+            fast = next(fast, arr);
+            fast = next(fast, arr);
+            // System.out.println(slow+" "+fast);
+            if (slow == -1 || fast == -1)
+                return false;
+            // if(ct==1 && fast==slow) return false;
+            if (fast == slow)
+                return true;
+        }
+        // System.out.println("false");
+        return false;
     }
 
-    private int getIndex(int input, int[] nums) {
-        int len = nums.length;
-        if (input >= 0)
-            return input % len;
-        else
-            return len - (len - input) % len;
+    public int next(int itr, int[] arr) {
+        int ans = (itr + arr[itr] + 1000 * arr.length) % arr.length;
+        return (ans == itr) ? -1 : ans;
     }
 }
