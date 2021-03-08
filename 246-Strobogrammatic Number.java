3eb2be6478890d21d53f70f85b601842f103091f
jing <jip45@pitt.edu>
2021-03-08 10:41:00 -0500

    20210308

--- 246-Strobogrammatic Number.java
+++ 246-Strobogrammatic Number.java
@@ -1,7 +1,8 @@
 import java.util.*;
+
 class StrobogrammaticNumber {
 
-    // HashMap 
+    // HashMap
     public boolean isStrobogrammatic(String num) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 0);
@@ -19,5 +20,26 @@ class StrobogrammaticNumber {
             }
         }
         return sb.toString().equals(num);
+    }
+
+    // HashMap + Two Pointers.
+    public boolean isStrobogrammaticI(String num) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('8', '8');
+        map.put('1', '1');
+        map.put('0', '0');
+
+        int i = 0;
+        int j = num.length() - 1;
+        while (i <= j) {
+            if (!map.containsKey(num.charAt(i)) || num.charAt(j) != map.get(num.charAt(i))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
