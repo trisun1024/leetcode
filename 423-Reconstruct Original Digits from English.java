70dc51cb4b35abc79e67bfb02984f7706bd0f9d4
jing <jip45@pitt.edu>
2021-03-28 17:56:21 -0400

    20210328

--- 423-Reconstruct Original Digits from English.java
+++ 423-Reconstruct Original Digits from English.java
@@ -3,32 +3,64 @@ import java.util.*;
 class ReconstructOriginalDigitsfromEnglish {
 
     // Array
+    // zero, one, two, three, four, five, six, seven, eight, nine
     public String originalDigits(String s) {
-        int[] count = new int[128];
+        int[] count = new int[26];
         for (char c : s.toCharArray()) {
-            count[c]++;
+            count[c - 'a']++;
         }
         int[] nums = new int[10];
-        nums[0] = count['z'];
-        nums[2] = count['w'];
-        nums[4] = count['u'];
-        nums[6] = count['x'];
-        nums[8] = count['g'];
-        nums[1] = count['o'] - nums[0] - nums[2] - nums[4];
-        nums[3] = count['h'] - nums[8];
-        nums[5] = count['f'] - nums[4];
-        nums[7] = count['v'] - nums[5];
-        nums[9] = count['i'] - nums[5] - nums[6] - nums[8];
+        // unique character mapping with numbers
+        nums[0] = count['z' - 'a'];
+        nums[2] = count['w' - 'a'];
+        nums[4] = count['u' - 'a'];
+        nums[6] = count['x' - 'a'];
+        nums[8] = count['g' - 'a'];
+        // characters appear in both numbers, then know the number of others
+        nums[3] = count['h' - 'a'] - nums[8];
+        nums[5] = count['f' - 'a'] - nums[4];
+        nums[7] = count['v' - 'a'] - nums[5];
+        nums[1] = count['o' - 'a'] - nums[0] - nums[2] - nums[4];
+        nums[9] = count['i' - 'a'] - nums[5] - nums[6] - nums[8];
 
-        StringBuilder ret = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < 10; ++i) {
-            if (nums[i] > 0) {
-                char[] arr = new char[nums[i]];
-                Arrays.fill(arr, (char) ('0' + i));
-                ret.append(String.valueOf(arr));
+            while (nums[i] > 0) {
+                res.append(String.valueOf(i));
+                nums[i]--;
             }
         }
 
-        return ret.toString();
+        return res.toString();
+    }
+
+    public String originalDigitsI(String s) {
+        int[] alphabets = new int[26];
+        for (char c : s.toCharArray()) {
+            alphabets[c - 'a']++;
+        }
+        int[] digits = new int[10];
+        // the character only appear in mapping numbers
+        digits[0] = alphabets['z' - 'a'];
+        digits[2] = alphabets['w' - 'a'];
+        digits[6] = alphabets['x' - 'a'];
+        digits[8] = alphabets['g' - 'a'];
+        // the character appear
+        digits[7] = alphabets['s' - 'a'] - digits[6];
+        digits[5] = alphabets['v' - 'a'] - digits[7];
+        digits[3] = alphabets['h' - 'a'] - digits[8];
+        digits[4] = alphabets['f' - 'a'] - digits[5];
+        // missing
+        digits[9] = alphabets['i' - 'a'] - digits[6] - digits[8] - digits[5];
+        digits[1] = alphabets['o' - 'a'] - digits[0] - digits[2] - digits[4];
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < 10; ++i) {
+            for (int count = 0; count < digits[i]; count++) {
+                res.append(i);
+            }
+        }
+
+        return res.toString();
     }
 }
