1c9d3e9ef38afc2a711098008721538d32c6bb4e
jing <jip45@pitt.edu>
2021-04-03 22:06:58 -0400

    20210403

--- 032-Longest Valid Parentheses.java
+++ 032-Longest Valid Parentheses.java
@@ -1,8 +1,58 @@
 import java.util.*;
 
-// Stack
-class LongestParentheses {
+class LongestValidParentheses {
+
+    // Stack using Pointers. Time = O(N); Space = O(1);
     public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+
+    // DP. Time = O(N); Space = O(N);
+    public int longestValidParenthesesII(String s) {
+        int max = 0;
+        int n = s.length();
+        int[] dp = new int[n];
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
+    // Stack. Time = O(N); Space = O(N);
+    public int longestValidParenthesesIII(String s) {
         Deque<Integer> stack = new ArrayDeque<Integer>();
         stack.offerFirst(-1);
         int max = 0;
@@ -48,5 +98,3 @@ class LongestParentheses {
         return maxlen;
     }
 }
-
-
