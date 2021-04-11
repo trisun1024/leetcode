885c96c3c93dac8dcf790f7ad59e1588ec1024ca
jing <jip45@pitt.edu>
2021-04-10 22:42:54 -0400

    20210410

--- 329-Longest Increasing Path in a Matrix.java
+++ 329-Longest Increasing Path in a Matrix.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class LongestIncreasingPathInMatrix {
 
     // DFS + memorization. Time = O(M*N); Space = O(M*N);
@@ -30,5 +32,63 @@ class LongestIncreasingPathInMatrix {
             }
         }
         return ++memo[i][j];
+    }
+
+    // Topological Sorting. Time = O(M*N); Space = O(M*N);
+    public int longestIncreasingPathI(int[][] grid) {
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        // padding the matrix with zero as boundaries
+        // assuming all positive integer, otherwise use INT_MIN as boundaries
+        int[][] matrix = new int[m + 2][n + 2];
+        for (int i = 0; i < m; ++i) {
+            System.arraycopy(grid[i], 0, matrix[i + 1], 1, n);
+        }
+
+        // calculate outdegrees
+        int[][] outdegree = new int[m + 2][n + 2];
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                for (int[] d : DIRS) {
+                    if (matrix[i][j] < matrix[i + d[0]][j + d[1]]) {
+                        outdegree[i][j]++;
+                    }
+                }
+            }
+        }
+
+        // find leaves who have zero out degree as the initial level
+        n += 2;
+        m += 2;
+        List<int[]> leaves = new ArrayList<>();
+        for (int i = 1; i < m - 1; ++i) {
+            for (int j = 1; j < n - 1; ++j) {
+                if (outdegree[i][j] == 0) {
+                    leaves.add(new int[] { i, j });
+                }
+            }
+        }
+
+        // remove leaves level by level in topological order
+        int height = 0;
+        while (!leaves.isEmpty()) {
+            height++;
+            List<int[]> newLeaves = new ArrayList<>();
+            for (int[] node : leaves) {
+                for (int[] d : DIRS) {
+                    int x = node[0] + d[0], y = node[1] + d[1];
+                    if (matrix[node[0]][node[1]] > matrix[x][y]) {
+                        if (--outdegree[x][y] == 0) {
+                            newLeaves.add(new int[] { x, y });
+                        }
+                    }
+                }
+            }
+            leaves = newLeaves;
+        }
+        return height;
     }
 }
